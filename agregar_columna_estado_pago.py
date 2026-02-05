841e185258a40dc699627670a24b1cf94056e19b
Angel Sanabria <sanabriaangel9718@gmail.com>
2026-02-05 20:37:41 -0300

    estado pago

---
+++ agregar_columna_estado_pago.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+# Datos de conexión
+conn = psycopg2.connect(
+    dbname="jugueteria_db",
+    user="postgres",
+    password="123456",
+    host="localhost",
+    port=5432
+)
+
+with conn:
+    with conn.cursor() as cur:
+        cur.execute("ALTER TABLE notas_debito ADD COLUMN estado_pago VARCHAR(50);")
+
+print("Columna 'estado_pago' agregada correctamente a notas_debito.")
