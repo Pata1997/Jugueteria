2fefed9f0511814d81ae1a17400626557b3c8cec
Angel Sanabria <sanabriaangel9718@gmail.com>
2026-02-05 20:40:23 -0300

    estado fecha

---
+++ agregar_columna_fecha_modificacion.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+# Datos de conexión
+conn = psycopg2.connect(
+    dbname="jugueteria_db",
+    user="postgres",
+    password="123456",
+    host="localhost",
+    port=5432
+)
+
+with conn:
+    with conn.cursor() as cur:
+        cur.execute("ALTER TABLE notas_debito ADD COLUMN fecha_modificacion TIMESTAMP;")
+
+print("Columna 'fecha_modificacion' agregada correctamente a notas_debito.")
