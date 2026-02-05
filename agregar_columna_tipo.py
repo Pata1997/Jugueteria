312a48524949ec5812003bb3d3f0fe124373d822
Angel Sanabria <sanabriaangel9718@gmail.com>
2026-02-05 20:31:52 -0300

    tipo

---
+++ agregar_columna_tipo.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+# Datos de conexión
+conn = psycopg2.connect(
+    dbname="jugueteria_db",
+    user="postgres",
+    password="123456",
+    host="localhost",
+    port=5432
+)
+
+with conn:
+    with conn.cursor() as cur:
+        cur.execute("ALTER TABLE notas_debito ADD COLUMN tipo VARCHAR(50);")
+
+print("Columna 'tipo' agregada correctamente a notas_debito.")
